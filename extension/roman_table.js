591966e2864a732ffdcb3d707d234368eab0e0c1
Satoshi Fukutomi <info@fuktommy.com>
2025-01-01 17:19:46 +0900

    zenkaku exclamation and question

--- extension/roman_table.js
+++ extension/roman_table.js
@@ -26,6 +26,8 @@ var romanTable = {
   '[': '\uff62', // 「
   ']': '\uff63', // 」
   '-': '\u30fc', // ー
+  '!': '\uff01', // ！
+  '?': '\uff1f', // ？
 
   // The following rule comes from https://github.com/skk-dev/ddskk/blob/8c47f46e38a29a0f3eabcd524268d20573102467/docs/06_apps.rst?plain=1#L2100-L2137
   'z ': '\u3000', // 全角スペース
