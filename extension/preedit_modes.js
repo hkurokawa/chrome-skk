8ba955536588de1abfa84ce5c654fcc509ad87ab
Tamotsu Takahashi <ttakah+github@gmail.com>
2025-12-21 00:14:05 +0900

    Convert 'n' to 'ん' before commitText, Fix #68
    Enter, q, l and L will commitText(preedit + nn)

--- extension/preedit_modes.js
+++ extension/preedit_modes.js
@@ -11,8 +11,10 @@ function initPreedit(skk) {
 }
 
 function preeditKeybind(skk, keyevent) {
+  const nn = (skk.roman == 'n') ? romanTable['nn'] : '';
+
   if (keyevent.key == 'Enter' || (keyevent.key == 'j' && keyevent.ctrlKey)) {
-    skk.commitText(skk.preedit);
+    skk.commitText(skk.preedit + nn);
     skk.preedit = '';
     skk.roman = '';
     skk.switchMode('hiragana');
@@ -58,7 +60,7 @@ function preeditKeybind(skk, keyevent) {
   }
 
   if (keyevent.key == 'q' && skk.currentMode != 'ascii-preedit') {
-    skk.commitText(kanaTurnOver(skk.preedit));
+    skk.commitText(kanaTurnOver(skk.preedit + nn));
     skk.preedit = '';
     skk.roman = '';
     skk.switchMode('hiragana');
@@ -66,7 +68,7 @@ function preeditKeybind(skk, keyevent) {
   }
 
   if (keyevent.key == 'l' && skk.currentMode != 'ascii-preedit') {
-    skk.commitText(skk.preedit);
+    skk.commitText(skk.preedit + nn);
     skk.preedit = '';
     skk.roman = '';
     skk.switchMode('ascii');
@@ -74,7 +76,7 @@ function preeditKeybind(skk, keyevent) {
   }
 
   if (keyevent.key == 'L' && skk.currentMode != 'ascii-preedit') {
-    skk.commitText(skk.preedit);
+    skk.commitText(skk.preedit + nn);
     skk.preedit = '';
     skk.roman = '';
     skk.switchMode('full-ascii');
